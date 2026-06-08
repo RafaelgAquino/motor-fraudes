c0e062dc2ba4c2a521e74243962a575f120a03fd
Rafael Aquino <rafael.aquino@seduh.local>
2026-06-08 16:59:52 -0300

    fix: corrige o lugar da senha e email de verdade

--- front-motor-fraudes/src/app/login/login.ts
+++ front-motor-fraudes/src/app/login/login.ts
@@ -8,15 +8,33 @@ import { Router } from '@angular/router';
 })
 export class Login {
   
-  // O motorista contratado logo no início (inpedindo o pisca-pisca)
   constructor(private router: Router) { }
 
-  fazerLogin(email: string) {
-    if (email !== '') {
-      // Navegação instantânea
-      this.router.navigate(['/pix']);
-    } else {
-      alert('⚠️ Por favor, digite seu e-mail corporativo.');
+  async fazerLogin(emailDigitado: string, senhaDigitada: string) {
+    // 1. Verifica se o usuário não deixou nada em branco
+    if (!emailDigitado || !senhaDigitada) {
+      alert('⚠️ Por favor, preencha seu e-mail e senha.');
+      return;
+    }
+
+    try {
+      // 2. Bate na porta do Java (Render) para verificar as credenciais
+      const resposta = await fetch('https://bradesco-sec-java.onrender.com/clientes/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email: emailDigitado, senha: senhaDigitada })
+      });
+
+      // 3. O Java respondeu!
+      if (resposta.ok) {
+        // Sucesso! A senha bate com o banco de dados.
+        this.router.navigate(['/pix']);
+      } else {
+        // Erro 401 ou 404: Senha errada ou usuário não existe
+        alert('❌ E-mail ou senha incorretos! Tente novamente.');
+      }
+    } catch (erro) {
+      alert('💥 Falha de comunicação com o servidor de segurança.');
     }
   }
 
