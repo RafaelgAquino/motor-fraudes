ead03488c06cdf75d27f94c6135fceba6752b628
Rafael Aquino <rafael.aquino@seduh.local>
2026-06-08 16:06:35 -0300

    feat: adiciona validacao de confirmacao de senha

--- front-motor-fraudes/src/app/cadastro/cadastro.ts
+++ front-motor-fraudes/src/app/cadastro/cadastro.ts
@@ -15,16 +15,26 @@ export class Cadastro {
   }
 
   // A sua função principal de salvar, reconectada!
-  async salvarCadastro(nomeDigitado: string, idadeDigitada: string, rendaDigitada: string, email: string) {
+  async salvarCadastro(nomeDigitado: string, idadeDigitada: string, rendaDigitada: string, email: string, senhaDigitada: string, confirmarSenhaDigitada: string) {
+    
+    // 1. Validação da Senha
+    if (senhaDigitada !== confirmarSenhaDigitada) {
+      alert("⚠️ Ops! As senhas não coincidem. Digite novamente.");
+      return; 
+    }
+
+    // 2. Montando o Pacote Limpo
     const payload = {
       nome: nomeDigitado,
       idade: parseInt(idadeDigitada),
       renda: parseFloat(rendaDigitada),
-      email: email
-      };
+      email: email,
+      senha: senhaDigitada
+    };
 
     try {
-      const resposta = await fetch('http://localhost:8080/clientes', {
+      // 3. Enviando para o Cérebro (Render)
+      const resposta = await fetch('https://bradesco-sec-java.onrender.com/clientes', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(payload)
@@ -32,15 +42,12 @@ export class Cadastro {
 
       if (resposta.ok) {
         alert(`✅ Sucesso! O perfil comportamental de ${payload.nome} foi salvo no Banco de Dados!`);
-        
-        // Redirecionamento instantâneo para a área logada (PIX)! 🚀
-        this.router.navigate(['/pix']); 
-        
+        this.router.navigate(['/login']); // Redireciona para o login após o sucesso
       } else {
-        alert('❌ Erro: O Cérebro Java recusou os dados. Status: ' + resposta.status);
+        alert('❌ Erro ao salvar o cadastro.');
       }
     } catch (erro) {
-      alert('💥 Falha total de conexão. O servidor Java (IntelliJ) está ligado?');
+      alert('💥 Falha de comunicação com o servidor.');
     }
   }
 }
