75e50e36c32e6afec706482ad01fd7a9cedd89ea
Rafael Aquino <rafael.aquino@seduh.local>
2026-06-08 16:53:20 -0300

    fix: corrige o login e autenticação

--- motor-fraudes-api/src/main/java/com/bradesco/motor_fraudes_api/controller/ClienteController.java
+++ motor-fraudes-api/src/main/java/com/bradesco/motor_fraudes_api/controller/ClienteController.java
@@ -1,8 +1,10 @@
-package com.bradesco.motor_fraudes_api.controller; // Mantenha a sua linha de package original!
+package com.bradesco.motor_fraudes_api.controller;
 
 import com.bradesco.motor_fraudes_api.model.Cliente;
 import com.bradesco.motor_fraudes_api.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;       // 👈 Olha os imports certinhos aqui no topo!
+import org.springframework.http.ResponseEntity;   // 👈
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,20 +13,32 @@ import org.springframework.web.bind.annotation.*;
 public class ClienteController {
 
     @Autowired
-    private ClienteRepository repository;
+    private ClienteRepository repository; // O nome oficial é "repository"
 
     @PostMapping
     public Cliente salvarNovoCliente(@RequestBody Cliente novoCliente) {
         System.out.println("🚨 RECEBIDO DO FRONT-END: " + novoCliente.getNome());
-
-        // Salva no banco de dados do PostgreSQL e já devolve o cliente com o ID preenchido
         return repository.save(novoCliente);
     }
 
-    // 👇 A Porta de Saída (Para você ver os cadastros no navegador)
     @GetMapping
     public java.util.List<Cliente> listarTodosOsClientes() {
-        // O Java vai no banco, pega a tabela inteira e devolve como uma lista!
         return repository.findAll();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> fazerLogin(@RequestBody Cliente dadosLogin) {
+        // Agora usando o nome correto "repository" na hora de buscar!
+        for (Cliente clienteSalvo : repository.findAll()) {
+            if (clienteSalvo.getEmail().equals(dadosLogin.getEmail()) &&
+                    clienteSalvo.getSenha().equals(dadosLogin.getSenha())) {
+
+                // Achou! Pode abrir a porta (Retorna status 200 OK)
+                return ResponseEntity.ok().body("{\"mensagem\": \"Acesso Permitido\"}");
+            }
+        }
+
+        // Se o loop terminar e não achar ninguém, bloqueia a porta
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("{\"erro\": \"Credenciais inválidas\"}");
     }
 }
